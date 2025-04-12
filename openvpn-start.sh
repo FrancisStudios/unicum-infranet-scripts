3588f2df3d732c37b5489c1aa5e799ebac07d395
Francis <vlfmap@gmail.com>
2025-04-12 14:00:05 +0200

    openvpn start script

---
+++ openvpn-start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo -e "\e[45mAttempting to start OpenVPN\e[0m"
+
+# Locate OpenVPN folder
+cd /etc/openvpn/server
+
+# Launch executable with server config
+openvpn --config /etc/openvpn/server.conf
+
+echo -e "\e[45mOpenVPN starting\e[0m"
