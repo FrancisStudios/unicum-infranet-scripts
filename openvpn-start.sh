09bdb023a883be645dafbb6276100e83bb9a72e1
Francis <vlfmap@gmail.com>
2025-04-12 14:18:05 +0200

    sf

--- openvpn-start.sh
+++ openvpn-start.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 echo -e "\e[45mAttempting to start OpenVPN\e[0m"
+echo -e "\e[0;34m Please login Francis"
+
+sudo francis
+
+echo -e "\e[0m"
 
 # Locate OpenVPN folder
 cd /etc/openvpn/server
